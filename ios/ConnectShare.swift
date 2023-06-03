f730429117dcb118322cdcf599c59b33f1270b94
mohamad-n <alfymohamad@gmail.com>
2023-06-04 02:09:08 +0330

    add ios  libs

---
+++ ios/ConnectShare.swift
@@ -0,0 +1,8 @@
+//
+//  ConnectShare.swift
+//  VPNGATE
+//
+//  Created by mohamad on 6/4/23.
+//
+
+import Foundation
---
+++ ios/configurator.swift
@@ -0,0 +1,8 @@
+//
+//  configurator.swift
+//  VPNGATE
+//
+//  Created by mohamad on 6/4/23.
+//
+
+import Foundation
